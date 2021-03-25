67f1798b313d4049cb4a0d4fefe4308083f48ff6
Bader Alrasheed <badran1996@gmail.com>
2021-03-25 16:15:04 +0300

    Initial Commit

---
+++ Baims Landing Page/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Baims Landing Page
+//
+//  Created by Bader Alrasheed on 25/03/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
